c527b06f9d0831ec268cef11ec2ec304daccb9b0
ShopAnimal <kimnguyet0815@gmail.com>
2020-07-22 20:41:18 +0700

    Updates

---
+++ precache-manifest.ae643f0d9bf14cc9c14c1c43cb625833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec48bfad1cdc3588788357f512159a4",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "47280c512e21eb0066a8",
+    "url": "/home/static/css/main.9421bc17.chunk.css"
+  },
+  {
+    "revision": "6b20b20935c63e2b8001",
+    "url": "/home/static/js/2.93f220bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/home/static/js/2.93f220bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47280c512e21eb0066a8",
+    "url": "/home/static/js/main.c8754543.chunk.js"
+  },
+  {
+    "revision": "9ea9e2220a62fe046ca0",
+    "url": "/home/static/js/runtime-main.869225a8.js"
+  }
+]);
